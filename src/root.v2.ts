09471139ca7f2a8ded548d29f31a629e41144b8f
Michael Doye <michaeldoye@gmail.com>
2018-08-08 22:53:58 +0200

    card schema

--- src/root.v2.ts
+++ src/root.v2.ts
@@ -27,12 +27,54 @@ export const schemaV2 = buildSchema(`
     data: [Card]
   }
 
+  type Images {
+    small: String
+    normal: String
+    large: String
+    png: String
+  }
+
+  type Legalities {
+    standard: String
+    future: String
+    frontier: String
+    modern: String
+    legacy: String
+    pauper: String
+    vintage: String
+    penny: String
+    commander: String
+    duel: String
+    brawl: String
+  }
+
+  type PurchaseLinks {
+    amazon: String
+    ebay: String
+    tcgplayer: String
+  }
+
   type Card {
     name: String
     type_line: String
     printed_name: String
     usd: String
     lang: String
+    image_uris: Images
+    highres_image: Boolean
+    id: String
+    multiverse_ids: [Int]
+    mana_cost: String
+    cmc: Int
+    oracle_text: String
+    colors: [String]
+    color_identity: [String]
+    legalities: Legalities
+    set: String
+    set_name: String
+    rarity: String
+    purchase_uris: PurchaseLinks
+    collector_number: Int
   }
 
   type Query {
